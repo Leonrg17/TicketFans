ef3b784ba882a8b4addf91e1552c25a5491e052e
Leon <leon.gallardo@sjsu.edu>
2022-05-10 14:24:29 -0700

    update3

--- src/contracts/TicketFans.sol
+++ src/contracts/TicketFans.sol
@@ -10,7 +10,7 @@ contract TicketFan{
         uint id;
         string artist;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
 
@@ -18,7 +18,14 @@ contract TicketFan{
 event TicketCreated(uint id,
         string artist,
         uint price,
-        address owner,
+        address payable owner,
+        bool purchased
+        );
+
+event TicketPurchased(uint id,
+        string artist,
+        uint price,
+        address payable owner,
         bool purchased
         );
 
@@ -35,13 +42,38 @@ require(bytes(_artist).length > 0);
 require(_price>0);
 
 
-
-
 ticketCount++;//intement ticket count
 
 tickets[ticketCount] = Ticket(ticketCount,_artist,_price, msg.sender,false );//create ticket
 
 emit TicketCreated (ticketCount,_artist,_price, msg.sender,false);
+
+
+}
+
+function purchaseTicket(uint _id) public payable{
+
+Ticket memory _ticket = tickets[_id];//get ticket
+
+address payable _seller = _ticket.owner;//get owner
+
+require(_ticket.id >0 && _ticket.id <= ticketCount);//valid id
+
+require(msg.value >= _ticket.price);
+
+require(!_ticket.purchased);
+
+require(_seller != msg.sender);
+
+_ticket.owner = msg.sender;//give owner ship
+
+_ticket.purchased = true; //confirm purchase
+
+tickets[_id] = _ticket;//update ticket
+
+address(_seller).transfer(msg.value);//[pay seller]
+
+emit TicketPurchased (ticketCount,_ticket.artist,_ticket.price, msg.sender,true);
 
 
 }
--- test/TicketFans.test.js
+++ test/TicketFans.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const TicketFan = artifacts.require('/mnt/c/Users/leon_/.VScodeWorkspace/CMPE183TicketFans/src/contracts/TicketFans.sol')
 
 require('chai')
@@ -44,6 +46,57 @@ describe ('tickets', async () => {
         // FAILURE: Product must have a name
         await await ticketfans.createTicket('Kanye East', 0, { from: seller }).should.be.rejected;
       })
+
+      it('lists tickets', async () => {
+
+        const tick = await ticketfans.tickets(ticketCount)
+        assert.equal(tick.id.toNumber(), ticketCount.toNumber(), 'id is correct')
+        assert.equal(tick.artist, 'Kanye East', 'name is correct')
+        assert.equal(tick.price, '1000000000000000000', 'price is correct')
+        assert.equal(tick.owner, seller, 'owner is correct')
+        assert.equal(tick.purchased, false, 'purchased is correct')
+  
+
+      })
+
+      it('lists sells', async () => {
+        let selleroldbal
+        selleroldbal = await web3.eth.getBalance(seller)
+        selleroldbal = new web3.utils.BN(selleroldbal)
+
+
+        result = await ticketfans.purchaseTicket(ticketCount,{from:buyer, value:web3.utils.toWei('1', 'Ether')})
+
+        //ensure transaction went through
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), ticketCount.toNumber(), 'id is correct')
+        assert.equal(event.artist, 'Kanye East', 'name is correct')
+        assert.equal(event.price, '1000000000000000000', 'price is correct')
+        assert.equal(event.owner, buyer, 'owner is correct')
+        assert.equal(event.purchased, true, 'purchased is correct')
+  
+        //make sure payment has been recieved
+        let sellernewbal
+        sellernewbal = await web3.eth.getBalance(seller)
+        sellernewbal = new web3.utils.BN(sellernewbal)
+
+        let price
+        price = web3.utils.toWei('1','Ether')
+        price  = new web3.utils.BN(price)
+
+       const expectedbal = selleroldbal.add(price)
+
+       assert.equal(sellernewbal.toString(),expectedbal.toString())
+
+       await ticketfans.purchaseTicket(99,{from:buyer, value:web3.utils.toWei('1', 'Ether')}).should.be.rejected;//attempt to pruchase invalid ticket
+
+       await ticketfans.purchaseTicket(ticketCount,{from:buyer, value:web3.utils.toWei('.5', 'Ether')}).should.be.rejected;//not enough funds
+
+       await ticketfans.purchaseTicket(ticketCount,{from:deployer, value:web3.utils.toWei('1', 'Ether')}).should.be.rejected;//cannot be purchased twice
+
+       await ticketfans.purchaseTicket(ticketCount,{from:buyer, value:web3.utils.toWei('1', 'Ether')}).should.be.rejected;// buyer cannot be seller
+      })
+
 })
 
 })
